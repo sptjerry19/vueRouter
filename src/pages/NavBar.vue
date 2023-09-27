db38741474c26b65f99e240aef35e34ef6d416f3
sptjerry19 <duylinhvnu@gmail.com>
2023-09-27 09:23:59 +0700

    vue Router

--- src/pages/NavBar.vue
+++ src/pages/NavBar.vue
@@ -1,4 +1,6 @@
 <template>
-  <router-link to="/">Home</router-link>
-  <router-link to="/about">About</router-link>
+  <div class="absolute top-10 left-auto right-auto">
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </div>
 </template>
